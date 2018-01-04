26239e3916b77e1803d62b8aeca6dc0321717532
Lukas Werling <lukas.werling@gmail.com>
2018-01-04 17:36:54 +0100

    netpuncher-server: Add prometheus metrics endpoint

--- cmd/netpuncher-server/main.go
+++ cmd/netpuncher-server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net"
+	"net/http"
 	"os"
 	"os/signal"
 	"strconv"
@@ -10,7 +11,41 @@ import (
 	"github.com/lluchs/netpuncher"
 	"github.com/lluchs/netpuncher/c4netioudp"
 	"github.com/lluchs/netpuncher/server"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var (
+	connectionCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "netpuncher_connections_total",
+		Help: "Number of connections to the netpuncher",
+	})
+	currentConnectionsGauge = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "netpuncher_connections",
+		Help: "Number of clients currently connected to the netpuncher",
+	})
+	hostCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "netpuncher_hosts_total",
+		Help: "Number of hosts which registered with the netpuncher",
+	})
+	creqCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "netpuncher_creq_total",
+		Help: "Number of CReq messages processed by the netpuncher",
+	})
+	errorCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "netpuncher_errors_total",
+		Help: "Number of non-fatal errors during packet handling",
+	}, []string{"reason"})
+)
+
+func init() {
+	prometheus.MustRegister(connectionCounter)
+	prometheus.MustRegister(currentConnectionsGauge)
+	prometheus.MustRegister(hostCounter)
+	prometheus.MustRegister(creqCounter)
+	prometheus.MustRegister(errorCounter)
+}
 
 func main() {
 	listenaddr := net.UDPAddr{IP: net.IPv6unspecified, Port: 11115}
@@ -24,24 +59,32 @@ func main() {
 				log.Fatal("error during Accept: ", err)
 			}
 			log.Printf("connect: %v #%d\n", c.NetIOConn.RemoteAddr(), c.ID)
+			connectionCounter.Inc()
+			currentConnectionsGauge.Inc()
 		},
 		MarshalErr: func(err error) {
 			log.Println(err)
+			errorCounter.With(prometheus.Labels{"reason": "marshal"}).Inc()
 		},
 		UnsupportedVersionErr: func(c *server.Conn, err *netpuncher.ErrUnsupportedVersion) {
 			log.Printf("client #%d: unsupported version %d", c.ID, err)
+			errorCounter.With(prometheus.Labels{"reason": "unsupported version"}).Inc()
 		},
 		InvalidPacketErr: func(c *server.Conn, err error) {
 			log.Printf("client #%d: couldn't read packet: %v", c.ID, err)
+			errorCounter.With(prometheus.Labels{"reason": "invalid packet"}).Inc()
 		},
 		RegisterHost: func(host *server.Conn) {
 			log.Printf("host: #%d", host.ID)
+			hostCounter.Inc()
 		},
 		CReq: func(host *server.Conn, client *server.Conn) {
 			log.Printf("CReq: client %v <--> host %v #%d\n", client.NetIOConn.RemoteAddr(), host.NetIOConn.RemoteAddr(), host.ID)
+			creqCounter.Inc()
 		},
 		CloseConn: func(c *server.Conn, err *c4netioudp.ErrConnectionClosed) {
 			log.Printf("close:   %v #%d (%s)\n", c.NetIOConn.RemoteAddr(), c.ID, err)
+			currentConnectionsGauge.Dec()
 		},
 	}
 
@@ -51,6 +94,12 @@ func main() {
 	}
 	defer server.Close()
 	log.Printf("netpuncher listening on %v", server.Addr())
+
+	if addr := os.Getenv("METRICS_ADDR"); addr != "" {
+		log.Printf("metrics listening on %s", addr)
+		http.Handle("/metrics", promhttp.Handler())
+		go log.Fatal(http.ListenAndServe(addr, nil))
+	}
 
 	// Wait for an interrupt. Without this special handling, the connection
 	// would not be closed properly.
