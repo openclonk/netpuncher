66bfbbe958eff5baafb027579e680ca8811fa347
Lukas Werling <lukas.werling@gmail.com>
2018-01-06 01:25:44 +0100

    Label Prometheus counters by IP protocol

--- cmd/netpuncher-server/main.go
+++ cmd/netpuncher-server/main.go
@@ -17,34 +17,45 @@ import (
 )
 
 var (
-	connectionCounter = prometheus.NewCounter(prometheus.CounterOpts{
+	connectionCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "netpuncher_connections_total",
 		Help: "Number of connections to the netpuncher",
-	})
-	currentConnectionsGauge = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "netpuncher_connections",
-		Help: "Number of clients currently connected to the netpuncher",
-	})
-	hostCounter = prometheus.NewCounter(prometheus.CounterOpts{
+	}, []string{"protocol"})
+	disconnectCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "netpuncher_disconnects_total",
+		Help: "Number of disconnects from the netpuncher",
+	}, []string{"protocol"})
+	hostCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "netpuncher_hosts_total",
 		Help: "Number of hosts which registered with the netpuncher",
-	})
-	creqCounter = prometheus.NewCounter(prometheus.CounterOpts{
+	}, []string{"protocol"})
+	creqCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "netpuncher_creq_total",
 		Help: "Number of CReq messages processed by the netpuncher",
-	})
+	}, []string{"protocol"})
 	errorCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "netpuncher_errors_total",
 		Help: "Number of non-fatal errors during packet handling",
-	}, []string{"reason"})
+	}, []string{"protocol", "reason"})
 )
 
 func init() {
 	prometheus.MustRegister(connectionCounter)
-	prometheus.MustRegister(currentConnectionsGauge)
+	prometheus.MustRegister(disconnectCounter)
 	prometheus.MustRegister(hostCounter)
 	prometheus.MustRegister(creqCounter)
 	prometheus.MustRegister(errorCounter)
+}
+
+func protocol(addr net.Addr) string {
+	if udpaddr, ok := addr.(*net.UDPAddr); ok {
+		if udpaddr.IP.To4() != nil {
+			return "IPv4"
+		} else {
+			return "IPv6"
+		}
+	}
+	return "unknown"
 }
 
 func main() {
@@ -58,9 +69,9 @@ func main() {
 			if err != nil {
 				log.Fatal("error during Accept: ", err)
 			}
-			log.Printf("connect: %v #%d\n", c.NetIOConn.RemoteAddr(), c.ID)
-			connectionCounter.Inc()
-			currentConnectionsGauge.Inc()
+			addr := c.NetIOConn.RemoteAddr()
+			log.Printf("connect: %v #%d\n", addr, c.ID)
+			connectionCounter.With(prometheus.Labels{"protocol": protocol(addr)}).Inc()
 		},
 		MarshalErr: func(err error) {
 			log.Println(err)
@@ -68,23 +79,28 @@ func main() {
 		},
 		UnsupportedVersionErr: func(c *server.Conn, err *netpuncher.ErrUnsupportedVersion) {
 			log.Printf("client #%d: unsupported version %d", c.ID, err)
-			errorCounter.With(prometheus.Labels{"reason": "unsupported version"}).Inc()
+			errorCounter.With(prometheus.Labels{"protocol": protocol(c.NetIOConn.RemoteAddr()), "reason": "unsupported version"}).Inc()
 		},
 		InvalidPacketErr: func(c *server.Conn, err error) {
 			log.Printf("client #%d: couldn't read packet: %v", c.ID, err)
-			errorCounter.With(prometheus.Labels{"reason": "invalid packet"}).Inc()
+			errorCounter.With(prometheus.Labels{"protocol": protocol(c.NetIOConn.RemoteAddr()), "reason": "invalid packet"}).Inc()
 		},
 		RegisterHost: func(host *server.Conn) {
 			log.Printf("host: #%d", host.ID)
-			hostCounter.Inc()
+			hostCounter.With(prometheus.Labels{"protocol": protocol(host.NetIOConn.RemoteAddr())}).Inc()
 		},
 		CReq: func(host *server.Conn, client *server.Conn) {
-			log.Printf("CReq: client %v <--> host %v #%d\n", client.NetIOConn.RemoteAddr(), host.NetIOConn.RemoteAddr(), host.ID)
-			creqCounter.Inc()
+			clientaddr := client.NetIOConn.RemoteAddr()
+			log.Printf("CReq: client %v <--> host %v #%d\n", clientaddr, host.NetIOConn.RemoteAddr(), host.ID)
+			// The client sends the CReq message, so label this by the client's
+			// protocol. In the usual case, the two protocols will be the same
+			// anyways.
+			creqCounter.With(prometheus.Labels{"protocol": protocol(clientaddr)}).Inc()
 		},
 		CloseConn: func(c *server.Conn, err *c4netioudp.ErrConnectionClosed) {
-			log.Printf("close:   %v #%d (%s)\n", c.NetIOConn.RemoteAddr(), c.ID, err)
-			currentConnectionsGauge.Dec()
+			addr := c.NetIOConn.RemoteAddr()
+			log.Printf("close:   %v #%d (%s)\n", addr, c.ID, err)
+			disconnectCounter.With(prometheus.Labels{"protocol": protocol(addr)}).Inc()
 		},
 	}
 
